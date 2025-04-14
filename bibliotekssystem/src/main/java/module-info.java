11f2dc705a2ac161081b53f3cb7859d76e794bb3
Jesper Holmberg <jesper.a.holmberg@gmail.com>
2025-04-15 01:29:04 +0200

    Förändringar på hemskärmen

--- bibliotekssystem/src/main/java/module-info.java
+++ bibliotekssystem/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.openjfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
     requires java.sql;
 
     opens org.openjfx to javafx.fxml;
