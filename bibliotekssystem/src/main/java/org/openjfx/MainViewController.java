b672fddfa2b1349bf290fafea6cc9cfef0a8b5f8
Jesper Holmberg <jesper.a.holmberg@gmail.com>
2025-04-15 15:12:10 +0200

    Förändringar för namn och hemskärms justeringar

---
+++ bibliotekssystem/src/main/java/org/openjfx/MainViewController.java
@@ -0,0 +1,27 @@
+package org.openjfx;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainViewController {
+
+    @FXML
+    private Label statusLabel;
+
+    @FXML
+    private void handleLogin() {
+        statusLabel.setText("Du är inloggad!");
+    }
+
+    @FXML
+    private void pressHome() throws IOException {
+        App.setRoot("MainView");
+    }
+
+    @FXML
+    private void pressButton() throws IOException {
+        System.out.println("Button pressed");
+    }
+}
